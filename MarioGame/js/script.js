d7e375c3a81ab41904d1506fb183eb4eefb0dc34
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-05-02 10:52:47 -0300

    Add files via upload

---
+++ MarioGame/js/script.js
@@ -0,0 +1,39 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const moeda = document.querySelector('.moeda')
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosicao = pipe.offsetLeft
+    const marioPosicao = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const moedaPosicao = moeda.offsetLeft
+
+    console.log(marioPosicao)
+
+    if (pipePosicao <= 120 && pipePosicao > 0 && marioPosicao < 80){
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosicao}px`
+
+        mario.src = 'img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px'
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosicao}px`
+
+        moeda.style.animation = 'none'
+        moeda.style.left = `${moedaPosicao}px`
+
+        clearInterval(loop)
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump)
